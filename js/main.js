48cecdfcae8d262c3e0f2340ba46566c02461f9c
khaled311 <k.sarhan311@gmail.com>
2019-09-10 20:05:05 +0200

    Edit

--- js/main.js
+++ js/main.js
@@ -189,3 +189,61 @@ try {
 } catch (error) {
   
 }
+
+try{
+
+  if($(window).width() < 992){
+    $(".members .row").addClass("owl-carousel");
+    $(".members .row").css("margin", "auto");
+  }else{
+    $(".members .row").removeClass("owl-carousel");
+  }
+
+  $('.members .owl-carousel').owlCarousel({
+    rtl:true,
+    autoplay: false,
+    loop:true,
+    // margin:5,
+    nav:false,
+    dots: true,
+    items: 1
+  });
+} catch(error){
+
+}
+
+$(function(){
+  jQuery('img.svg').each(function(){
+      var $img = jQuery(this);
+      var imgID = $img.attr('id');
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
+  
+      jQuery.get(imgURL, function(data) {
+          // Get the SVG tag, ignore the rest
+          var $svg = jQuery(data).find('svg');
+  
+          // Add replaced image's ID to the new SVG
+          if(typeof imgID !== 'undefined') {
+              $svg = $svg.attr('id', imgID);
+          }
+          // Add replaced image's classes to the new SVG
+          if(typeof imgClass !== 'undefined') {
+              $svg = $svg.attr('class', imgClass+' replaced-svg');
+          }
+  
+          // Remove any invalid XML tags as per http://validator.w3.org
+          $svg = $svg.removeAttr('xmlns:a');
+          
+          // Check if the viewport is set, else we gonna set it if we can.
+          if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+              $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+          }
+  
+          // Replace image with new SVG
+          $img.replaceWith($svg);
+  
+      }, 'xml');
+  
+  });
+});
